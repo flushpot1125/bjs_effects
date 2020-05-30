4f7f9d3d10441c490cd64c7d314f99134c467df1
flushpot1125 <crescentblue1122@gmail.com>
2020-05-30 15:26:09 +0900

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+//参考：https://qiita.com/10mi8o/items/2477f2640291f0ce6687
+
+
+const path = require('path');
+//const outputPath = path.resolve(__dirname, 'dist');
+const outputPath = path.resolve(__dirname, './');//distではなくプロジェクトのトップに変更
+module.exports = {
+    
+    entry: './src/index.js',//起点となるファイルのみを指定。src以下のファイルを個別に指定する必要はない
+    output: {
+        // バンドルしてmain.jsとして出力（これは実体として生成されないが、index.htmlなどで呼び出し記述が必要）
+        filename: 'main.js',
+        path: outputPath
+    },
+    // webpack-dev-serverを立ち上げた時のドキュメントルートを設定
+    // 
+    devServer: {
+        contentBase: outputPath,
+        watchContentBase: true,//html,cssなどに変更があればブラウザリロードを自動実行
+        port: 3000
+    }
+}
